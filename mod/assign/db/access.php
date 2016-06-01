01bb5b5fe0d54e241bb31a903ccd7c2888b30f99
Alex Walker <alex.walker@glasgow.ac.uk>
2016-06-01 15:58:29 +0100

    MDL-49673 mod_assign: option to hide grader

--- mod/assign/db/access.php
+++ mod/assign/db/access.php
@@ -178,5 +178,14 @@ $capabilities = array(
             'manager' => CAP_ALLOW
         )
     ),
+    'mod/assign:showhiddengrader' => array(
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_MODULE,
+        'archetypes' => array(
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        )
+    ),
 );
 
